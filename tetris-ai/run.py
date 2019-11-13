6c152dc8f071144f32197a59141b5199951e6dc2
SindreP <sindrhpa@stud.ntnu.no>
2019-11-13 18:21:51 +0100

    save try 3

--- tetris-ai/run.py
+++ tetris-ai/run.py
@@ -17,7 +17,7 @@ scores = [0]
 epsilon_stop_episode = 0
 
 # Run dqn with Tetris
-def dqn(saveAs=time.strftime("%Y_%m_%d_%H_%M_%S.h5"), loadmodelsrc="", doTrain=True, episodes=2000, render_after=1950, agent=None):
+def dqn(saveAs=time.strftime("%Y_%m_%d_%H_%M_%S.h5"), loadmodelsrc="", doTrain=True, episodes=2000, render_after=10000, agent=None):
     global scores, epsilon_stop_episode
     env = Tetris()
     #episodes = 2000 # standard=2000~, går sakte etter 1500
@@ -36,17 +36,18 @@ def dqn(saveAs=time.strftime("%Y_%m_%d_%H_%M_%S.h5"), loadmodelsrc="", doTrain=T
     render_delay = None
     activations = ['relu', 'relu', 'linear']
 
-    if loadmodelsrc:
-        agent = DQNAgent(env.get_state_size(),
-                         n_neurons=n_neurons, activations=activations,
-                         epsilon_stop_episode=epsilon_stop_episode, mem_size=mem_size,
-                         discount=discount, replay_start_size=replay_start_size)
-        agent.model = agent.loadModel(loadmodelsrc)
-    else:
-        agent = DQNAgent(env.get_state_size(),
-                         n_neurons=n_neurons, activations=activations,
-                         epsilon_stop_episode=epsilon_stop_episode, mem_size=mem_size,
-                         discount=discount, replay_start_size=replay_start_size)
+    if not agent:
+        if loadmodelsrc:
+            agent = DQNAgent(env.get_state_size(),
+                             n_neurons=n_neurons, activations=activations,
+                             epsilon_stop_episode=epsilon_stop_episode, mem_size=mem_size,
+                             discount=discount, replay_start_size=replay_start_size)
+            agent.model = agent.loadModel(loadmodelsrc)
+        else:
+            agent = DQNAgent(env.get_state_size(),
+                             n_neurons=n_neurons, activations=activations,
+                             epsilon_stop_episode=epsilon_stop_episode, mem_size=mem_size,
+                             discount=discount, replay_start_size=replay_start_size)
 
     log_dir = f'logs/tetris-nn={str(n_neurons)}-mem={mem_size}-bs={batch_size}-e={epochs}-{datetime.now().strftime("%Y%m%d-%H%M%S")}'
     log = CustomTensorBoard(log_dir=log_dir)
@@ -60,7 +61,7 @@ def dqn(saveAs=time.strftime("%Y_%m_%d_%H_%M_%S.h5"), loadmodelsrc="", doTrain=T
 
         if (render_every and episode % render_every == 0) or episode > render_after:
             render = True
-            print("Mean score this", render_every, ": ", mean(scores))
+            #print("Mean score this", render_every, ": ", mean(scores))
         else:
             render = False
 
@@ -101,14 +102,14 @@ def dqn(saveAs=time.strftime("%Y_%m_%d_%H_%M_%S.h5"), loadmodelsrc="", doTrain=T
 
     if not loadmodelsrc:
         agent.model.save(saveAs)
-        pickle.dump(agent, saveAs + ".pickle")
+        pickle.dump(agent, open(saveAs + ".pickle", "wb"))
 
 
 if __name__ == "__main__":
     num = 1
     for i in range(num):
         start = time.time()
-        dqn(saveAs="ABC.h5", episodes=1600)
+        dqn(saveAs="ABC.h5", episodes=2500, render_after=2450)
         end = time.time()
         maxi_score = max(scores[epsilon_stop_episode:])
         mean_after_epsilon = mean(scores[epsilon_stop_episode:])
@@ -122,5 +123,8 @@ if __name__ == "__main__":
         np.plot([x for x in range(len(scores))], scores)
         np.show()
 
-        dqn(agent=pickle.load("ABC.h5.pickle"), doTrain=False, episodes=50, render_after=1)
-        #dqn(loadmodelsrc="ABC.h5", doTrain=False, episodes=50, render_after=1)
+        dqn(agent=pickle.load(open("ABC.h5.pickle", "rb")), doTrain=False, episodes=50, render_after=1)
+        maxi_score = max(scores[epsilon_stop_episode:])
+        mean_after_epsilon = mean(scores[epsilon_stop_episode:])
+        print("Mean:", mean_after_epsilon)
+        print("best score:", maxi_score)
