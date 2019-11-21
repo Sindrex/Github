cf21b7b960c1b7764a24d89a98504a037b2da63c
SindreP <sindrhpa@stud.ntnu.no>
2019-11-21 20:34:48 +0100

    rename

--- tetris-ai/test.py
+++ tetris-ai/test.py
@@ -4,20 +4,20 @@ from numpy import array
 import matplotlib.pyplot as plt
 import numpy as np
 
-dqn(agent=pickle.load(open("Agent2_50000_10000_1.pickle", "rb")), doTrain=False, episodes=100, render_after=1, mem_size=0)
+#dqn(agent=pickle.load(open("Agent2_50000_10000_1.pickle", "rb")), doTrain=False, episodes=100, render_after=1, mem_size=0)
 
-# mean_width = 100
-# score_local_mean = []
-# scores = np.loadtxt('Agent2_50000_scores.txt', delimiter="\n")
-# plt.plot(scores)
-# for k in range(len(scores)):
-#     start = max(k - mean_width, 0)
-#     end = k + mean_width
-#     s = scores[start:end]
-#     score_local_mean.append(sum(s) / len(s))
-# plt.plot(score_local_mean)
-# plt.ylim(0, 1000)
-# plt.show()
+mean_width = 100
+score_local_mean = []
+scores = np.loadtxt('Agent2_1000050000_scores.txt', delimiter="\n")
+plt.plot(scores)
+for k in range(len(scores)):
+    start = max(k - mean_width, 0)
+    end = k + mean_width
+    s = scores[start:end]
+    score_local_mean.append(sum(s) / len(s))
+plt.plot(score_local_mean)
+plt.ylim(0, 5000)
+plt.show()
 
 #memsiz = [1, 1000, 10000, 25000, 50000, 75000, 100000, 125000, 150000, 175000, 200000, 225000, 250000]
 #times = [210, 198, 1657, 1536, 1236, 1127, 1285, 1292, 1426, 1444, 1343, 1452, 1433]
